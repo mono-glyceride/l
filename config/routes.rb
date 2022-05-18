7f65cd122e7df73ebf994cb0f0560088ee2e1687
mono-glyceride <glyceride.mono@gmail.com>
2022-05-18 14:55:44 +0000

    add：User_controllerのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 	
-  devise_for :users
   get 'lists/new'
   get 'lists/create'
   get 'lists/show'
   get 'lists/index'
 	root 'home#index'
+	
+	devise_for :users, controllers: {
+	  sessions: 'users/sessions'
+	}
 	
 end
