2b61a0e05af6164c7abfd3856e4022540a03d301
mono-glyceride <glyceride.mono@gmail.com>
2022-07-07 15:25:32 +0000

    fix Twitterログインのルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 	
 	devise_for :users, controllers: {
 	  sessions: 'users/sessions',
-	  :omniauth_callbacks => 'users/omniauth_callbacks'
+	  omniauth_callbacks: 'users/omniauth_callbacks'
 	}
 	
 end
