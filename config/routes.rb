6119116071ccce24e6604d780f9fbd9cca48fbcd
mono-glyceride <glyceride.mono@gmail.com>
2022-07-07 15:00:55 +0000

    add Twitterログインのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 	root 'home#index'
 	
 	devise_for :users, controllers: {
-	  sessions: 'users/sessions'
+	  sessions: 'users/sessions',
+	  :omniauth_callbacks => 'users/omniauth_callbacks'
 	}
 	
 end
