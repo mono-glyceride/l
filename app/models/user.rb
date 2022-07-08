faca39014eb9204d4a78a443da0d1764667ac579
mono-glyceride <glyceride.mono@gmail.com>
2022-07-08 15:34:44 +0000

    update Twitterプロフィール画像を取得時にサニタイジング

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,18 +9,21 @@ class User < ApplicationRecord
   # Twitter認証ログイン用
   # ユーザーの情報があれば探し、無ければ作成する
   def self.find_for_oauth(auth)
-    user = User.find_by(uid: auth.uid, provider: auth.provider)
+    provider = auth[:provider]
+    uid = auth[:uid]
+    name = auth[:info][:name]
+    image_url = auth[:info][:image]
+    uri = URI.parse(image_url) # パースする必要がある
+    image = uri.open
+    email = User.dummy_email(auth)
+    password = Devise.friendly_token[0, 20]
 
-    user ||= User.create!(
-      uid: auth.uid,
-      provider: auth.provider,
-      email: User.dummy_email(auth),
-      password: Devise.friendly_token[0, 20],
-      name: auth[:info][:name],
-      image: auth[:info][:image]
-    )
-
-    user
+    find_or_create_by(provider: provider, uid: uid) do |user|
+      user.name = user_name
+      user.email = email
+      user.password = password
+      user.image.attach(io: image, filename: "#{user.name}_profile.png")
+    end
   end
 
   # ダミーのメールアドレスを作成
